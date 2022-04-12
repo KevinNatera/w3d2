b05105aa3eb06bad695b0fd681114f080fe8d8c3
miker704 <miker704@gmail.com>
2022-04-12 14:08:28 -0400

    update @14:08

--- card.rb
+++ card.rb
@@ -2,12 +2,38 @@ class Card
     attr_reader :revealed, :value
     def initialize
         @revealed = false
-        @value = rand(1..14)
+        @value = rand(0..25)
+        
+    end
+
+    def hide
+        
+        @revealed=false if @revealed = true
+        
+    end
+
+    def revealed
+        @revealed = true if @revealed = false
+    end
+
+    def to_s
+        alpha = ("A".."Z").to_a
+        return alpha[@value]
 
     end
 
+    def ==(guess)
+        guess == self.to_s()
+    end
+
+
     
 
-
-
 end
+
+ a = Card.new
+ p a.value
+#  b = Card.new
+#  p b.value 
+
+ p a.to_s
